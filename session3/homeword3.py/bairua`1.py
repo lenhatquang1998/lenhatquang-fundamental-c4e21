f27f5ab2497e850afb39b18535dd674a87a91cf4
lenhatquang1998 <quangln62@gmail.com>
2018-08-24 23:48:21 +0700

    homeword3

---
+++ session3/homeword3.py/bairua`1.py
@@ -0,0 +1,24 @@
+from turtle import *
+shape("turtle")
+colors = ["red", "blue", "brown", "yellow", "grey"]
+for j in range(3):
+    pencolor("red")
+    forward(100)
+    left(120)
+for g in range(4):
+    pencolor("blue")
+    forward(100)
+    left(90)
+for i in range(5):
+    pencolor("brown")
+    forward(100)
+    left(72)
+for h in range(6):
+    pencolor("yellow")
+    forward(100)
+    left(60)
+for m in range(7):
+    pencolor("grey")
+    forward(100)
+    left(360/7)  
+mainloop()
